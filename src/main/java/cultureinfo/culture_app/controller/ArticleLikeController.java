afe7dc3eadb4b476005155c52ae373ab34c74ff4
LeeSungWon <mindlrae602@gmail.com>
2025-05-17 23:27:01 +0900

    refactor: controller에 있던 권한 로직 service로 이동

--- src/main/java/cultureinfo/culture_app/controller/ArticleLikeController.java
+++ src/main/java/cultureinfo/culture_app/controller/ArticleLikeController.java
@@ -5,22 +5,18 @@ import cultureinfo.culture_app.service.ArticleLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import cultureinfo.culture_app.security.SecurityUtil;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/articles")
 public class ArticleLikeController {
     private final ArticleLikeService articleLikeService;
-    private final SecurityUtil securityUtil;
 
     // 좋아요 토글
     @PostMapping("/{articleId}/like")
     public ResponseEntity<ArticleLikeDto> toggleLike(
             @PathVariable Long articleId) {
-        
-        Long memberId = securityUtil.getCurrentId();
-        ArticleLikeDto response = articleLikeService.toggleLike(articleId, memberId);
+        ArticleLikeDto response = articleLikeService.toggleLike(articleId);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/cultureinfo/culture_app/controller/CommentLikeController.java
+++ src/main/java/cultureinfo/culture_app/controller/CommentLikeController.java
@@ -1,7 +1,6 @@
 package cultureinfo.culture_app.controller;
 
 import cultureinfo.culture_app.dto.response.CommentLikeDto;
-import cultureinfo.culture_app.security.SecurityUtil;
 import cultureinfo.culture_app.service.CommentLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/comments")
 public class CommentLikeController {
     private final CommentLikeService commentLikeService;
-    private final SecurityUtil securityUtil;
 
     //좋아요 토글
     @PostMapping("/{commentId}/like")
     public ResponseEntity<CommentLikeDto> toggleLike(
             @PathVariable Long commentId) {
-        
-        Long memberId = securityUtil.getCurrentId();
-        CommentLikeDto response = commentLikeService.toggleLike(commentId, memberId);
+
+        CommentLikeDto response = commentLikeService.toggleLike(commentId);
         return ResponseEntity.ok(response);
     }
 }
