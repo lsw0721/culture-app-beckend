4881630fcbb58692aab39ac671743c4f46a236c3
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-05-17 21:47:11 +0900

    feat: id로 article 객체 가져오기

--- src/main/java/cultureinfo/culture_app/repository/ArticleRepository.java
+++ src/main/java/cultureinfo/culture_app/repository/ArticleRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
+    Optional<Article> findById(Long id);
 }
