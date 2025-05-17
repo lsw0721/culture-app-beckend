668c63ad26be11193f092fae184b94d603cca4ac
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-05-17 21:33:00 +0900

    feat: 로그인한 유저 id 가져오기

--- src/main/java/cultureinfo/culture_app/controller/ArticleLikeController.java
+++ src/main/java/cultureinfo/culture_app/controller/ArticleLikeController.java
@@ -5,18 +5,21 @@ import cultureinfo.culture_app.service.ArticleLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import cultureinfo.culture_app.security.SecurityUtil;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/articles")
 public class ArticleLikeController {
     private final ArticleLikeService articleLikeService;
+    private final SecurityUtil securityUtil;
+
     // 좋아요 토글
     @PostMapping("/{articleId}/like")
     public ResponseEntity<ArticleLikeDto> toggleLike(
-            @PathVariable Long articleId,
-            @RequestParam Long memberId) {
-
+            @PathVariable Long articleId) {
+        
+        Long memberId = securityUtil.getCurrentId();
         ArticleLikeDto response = articleLikeService.toggleLike(articleId, memberId);
         return ResponseEntity.ok(response);
     }
