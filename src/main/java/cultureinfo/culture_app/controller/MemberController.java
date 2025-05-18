1204ee61585d10f8e401830f614de02ac04231b5
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-05-18 23:41:20 +0900

    feat: 공지사항, 1대1 이메일 문의, 자신이 쓴 글 확인 기능 추가

--- src/main/java/cultureinfo/culture_app/controller/MemberController.java
+++ src/main/java/cultureinfo/culture_app/controller/MemberController.java
@@ -2,16 +2,23 @@ package cultureinfo.culture_app.controller;
 
 
 import cultureinfo.culture_app.dto.request.*;
+import cultureinfo.culture_app.dto.response.AnnouncementDto;
+import cultureinfo.culture_app.dto.response.ArticleDto;
 import cultureinfo.culture_app.dto.response.MemberDto;
 import cultureinfo.culture_app.dto.security.JwtToken;
 import cultureinfo.culture_app.security.JwtTokenProvider;
+import cultureinfo.culture_app.service.AnnouncementService;
+import cultureinfo.culture_app.service.ArticleService;
 import cultureinfo.culture_app.service.EmailService;
 import cultureinfo.culture_app.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +29,8 @@ MemberController {
     private final MemberService memberService;
     private final EmailService emailService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final ArticleService articleService;
+    private final AnnouncementService announcementService;
 
     // --- 회원가입 및 로그임 ---
 
@@ -98,6 +107,67 @@ MemberController {
             @Valid @RequestBody UpdatePasswordRequestDto req
     ) {
         memberService.updatePassword(req);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 자신이 쓴 게시글 확인
+    @GetMapping("/profile/myarticle")
+    public ResponseEntity<List<ArticleDto>> getMyArticles() {
+        List<ArticleDto> articles = articleService.getMyArticles();
+        return ResponseEntity.ok(articles);
+    }
+
+    //------------------공지사항--------------------
+    //공지사항 전체 조회
+    @GetMapping("/profile/announcement")
+    public ResponseEntity<List<AnnouncementDto>> getAllAnnouncements() {
+        List<AnnouncementDto> announcements = announcementService.getAllAnnouncements();
+        return ResponseEntity.ok(announcements);
+    }
+
+    //공지사항 단건 조회
+    @GetMapping("/profile/announcement/{id}")
+    public ResponseEntity<AnnouncementDto> getAnnouncement(@PathVariable Long id) {
+        AnnouncementDto announcement = announcementService.getAnnouncement(id);
+        return ResponseEntity.ok(announcement);
+    }
+
+    //공지사항 생성
+    @PostMapping("/profile/announcement")
+    public ResponseEntity<AnnouncementDto> createAnnouncement(
+            @RequestBody AnnouncementRequestDto requestDto) {
+
+        AnnouncementDto created = announcementService.createAnnouncement(requestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(created);
+    }
+
+    // 게시글 수정
+    @PutMapping("/profile/announcement/{id}")
+    public ResponseEntity<AnnouncementDto> updateAnnouncement(
+            @PathVariable Long id,
+            @RequestBody AnnouncementUpdateDto requestDto) {
+
+        AnnouncementDto updated = announcementService.updateAnnouncement(id, requestDto);
+        return ResponseEntity.ok(updated);
+    }
+
+    // 게시글 삭제
+    @DeleteMapping("/profile/announcement/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        articleService.deleteArticle(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //1:1 이메일 문의
+    //String으로 문의 제목, 내용 받아서 등록된 메일로 자기자신에게 보내기
+    //메일에 사용자 메일도 같이 첨부해서 보내기(관리자가 문의내용에 대한 답변을 사용자에게 개인적으로 보낼 수 있도록)
+    @PostMapping("/profile/inquiry")
+    public ResponseEntity<Void> inquiryEmail(
+            @RequestBody InquiryRequestDto requestDto) {
+
+        emailService.sendInquiry(requestDto);
         return ResponseEntity.noContent().build();
     }
 
