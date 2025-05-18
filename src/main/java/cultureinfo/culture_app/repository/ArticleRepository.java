9812fa094afc506d1dc615a471b99b117cef576f
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-05-18 23:34:30 +0900

    feat: 사용자가 작성한 article 가져오기
    repository

--- src/main/java/cultureinfo/culture_app/repository/ArticleRepository.java
+++ src/main/java/cultureinfo/culture_app/repository/ArticleRepository.java
@@ -4,9 +4,11 @@ import cultureinfo.culture_app.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
     Optional<Article> findById(Long id);
+    List<Article> findAllByMember_id(Long member_id);
 }
