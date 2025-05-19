fe2357fedba45dc8bc1dd940e8907d16c5901926
LeeSungWon <mindlrae602@gmail.com>
2025-05-20 02:14:22 +0900

    feat: 전체 조회 및 검색 시 무한스크롤 가능하게 slice 적용

--- src/main/java/cultureinfo/culture_app/repository/ArticleRepositoryCustom.java
+++ src/main/java/cultureinfo/culture_app/repository/ArticleRepositoryCustom.java
@@ -1,10 +1,14 @@
 package cultureinfo.culture_app.repository;
 
 import cultureinfo.culture_app.domain.Article;
-
-import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 //querydsl을 사용하기 위한 커스텀 저장소 정의
 public interface ArticleRepositoryCustom {
-    List<Article> searchByKeyword(String keyword);
+    //키워드 검색 + Slice(무한 스크롤용)
+    Slice<Article> searchByKeyword(String keyword, Pageable pageable);
+
+    //slice 적용한 전체 게시글 조회
+    Slice<Article> findAllBy(Pageable pageable);
 }
