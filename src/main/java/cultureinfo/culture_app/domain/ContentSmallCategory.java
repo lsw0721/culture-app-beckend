c1dda9a5a9035845b1fd92a96d412e33cf2db8a3
LeeSungWon <mindlrae602@gmail.com>
2025-05-22 00:12:58 +0900

    refactor: 소분류 이름 변수명 변경

--- src/main/java/cultureinfo/culture_app/domain/ContentSmallCategory.java
+++ src/main/java/cultureinfo/culture_app/domain/ContentSmallCategory.java
@@ -10,13 +10,14 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
+//콘텐츠 소분류
 public class ContentSmallCategory {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String ContentSmallCategoryName; // 콘텐츠 소분류 "2025 봄 대동제" 등
+    private String name; // 콘텐츠 소분류 "2025 봄 대동제" 등
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) // optional = false 조건은 연관관계가 무조건 있어야 함을 표시
     @JoinColumn(name = "content_subcategory_id")
     private ContentSubcategory contentSubcategory;
 
