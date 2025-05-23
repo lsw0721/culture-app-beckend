5ce7ff56cf27ca74f0ab367b1f12d75312a73ff0
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-05-24 00:11:25 +0900

    refactor: 어노테이션 변경

--- src/main/java/cultureinfo/culture_app/dto/request/MemberProfileEditRequestDto.java
+++ src/main/java/cultureinfo/culture_app/dto/request/MemberProfileEditRequestDto.java
@@ -4,6 +4,7 @@ package cultureinfo.culture_app.dto.request;
 import cultureinfo.culture_app.domain.type.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +17,7 @@ public class MemberProfileEditRequestDto {
     private String name;
     @Email @NotBlank
     private String email;
-    @NotBlank
+    @NotNull
     private Gender gender;
     @NotBlank
     private String location;
