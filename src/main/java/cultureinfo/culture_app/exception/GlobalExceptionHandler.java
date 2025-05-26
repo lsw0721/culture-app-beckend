a4aba36b8a5bc6c9410001b3d94b730435e521fe
LeeSungWon <mindlrae602@gmail.com>
2025-05-26 19:23:43 +0900

    refactor: 필요없는 정의 삭제

--- src/main/java/cultureinfo/culture_app/exception/GlobalExceptionHandler.java
+++ src/main/java/cultureinfo/culture_app/exception/GlobalExceptionHandler.java
@@ -1,13 +1,8 @@
 package cultureinfo.culture_app.exception;
-
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {// 파일 입출력 예외 처리
