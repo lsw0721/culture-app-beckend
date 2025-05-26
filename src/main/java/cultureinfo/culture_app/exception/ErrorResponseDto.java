49109992d13c525aaee0ae0aa066ee92a6f1a0ca
LeeSungWon <mindlrae602@gmail.com>
2025-05-26 19:24:05 +0900

    refactor: 에러의 code를 받도록 변경

--- src/main/java/cultureinfo/culture_app/exception/ErrorResponseDto.java
+++ src/main/java/cultureinfo/culture_app/exception/ErrorResponseDto.java
@@ -9,7 +9,7 @@ public class ErrorResponseDto {
     private final String code; //에러코드
     private final String message; //사용자에게 보여줄 메세지
 
-    public static ErrorResponseDto of(ErrorCode code) {
-        return new ErrorResponseDto(code.name(), code.getMessage());
+    public static ErrorResponseDto of(ErrorCode errorCode) {
+        return new ErrorResponseDto(errorCode.getCode(), errorCode.getMessage());
     }
 }
