40741f847b7efa758bb84cf0426970fc931f9741
LeeSungWon <mindlrae602@gmail.com>
2025-05-26 20:44:14 +0900

    refactor: 에러 메세지, 에러코드, 에러 발생시간, 에러 생긴 지점을 받을 수 있게 설정

--- src/main/java/cultureinfo/culture_app/exception/ErrorResponseDto.java
+++ src/main/java/cultureinfo/culture_app/exception/ErrorResponseDto.java
@@ -1,15 +1,25 @@
 package cultureinfo.culture_app.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ErrorResponseDto {
     private final String code; //에러코드
     private final String message; //사용자에게 보여줄 메세지
-
-    public static ErrorResponseDto of(ErrorCode errorCode) {
-        return new ErrorResponseDto(errorCode.getCode(), errorCode.getMessage());
+    private final LocalDateTime timestamp; // 에러 발생 시각
+    private final String path; // 요청 url
+    public static ErrorResponseDto of(ErrorCode errorCode, String path) {
+        return ErrorResponseDto.builder()
+                .code(errorCode.getCode()) // 에러 코드
+                .message(errorCode.getMessage()) // 에러 메세지
+                .timestamp(LocalDateTime.now()) // 에러 발생 시간
+                .path(path) // 에러 생긴 url
+                .build();
     }
 }
--- src/main/java/cultureinfo/culture_app/exception/GlobalExceptionHandler.java
+++ src/main/java/cultureinfo/culture_app/exception/GlobalExceptionHandler.java
@@ -1,25 +1,30 @@
 package cultureinfo.culture_app.exception;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {// 파일 입출력 예외 처리
     // 커스텀 예외
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException ex) {
+    public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException ex, HttpServletRequest request) {
         ErrorCode code = ex.getErrorCode();
         return ResponseEntity
                 .status(code.getStatus())
-                .body(ErrorResponseDto.of(code));
+                .body(ErrorResponseDto.of(code, request.getRequestURI()));
     }
+
+
 
     // 그 외 모든 예외
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDto> handleAll(Exception ex) {
+    public ResponseEntity<ErrorResponseDto> handleAll(Exception ex, HttpServletRequest request) {
+        log.error("Unhandled exception", ex); // 처리되지 않은 에러 로그 저장
         return ResponseEntity
                 .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-                .body(ErrorResponseDto.of(ErrorCode.INTERNAL_SERVER_ERROR));
+                .body(ErrorResponseDto.of(ErrorCode.INTERNAL_SERVER_ERROR, request.getRequestURI()));
     }
 }
