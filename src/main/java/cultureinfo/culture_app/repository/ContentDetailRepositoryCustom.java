7e5fcaee7607373bc603b2719d0070eb4d2c0d2a
LeeSungWon <mindlrae602@gmail.com>
2025-05-29 22:18:48 +0900

    feat: 탐색페이지 카테고리별 리스트업 기능을 위한 메소드 추가

--- src/main/java/cultureinfo/culture_app/repository/ContentDetailRepositoryCustom.java
+++ src/main/java/cultureinfo/culture_app/repository/ContentDetailRepositoryCustom.java
@@ -4,13 +4,21 @@ import cultureinfo.culture_app.dto.response.ContentSummaryDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
+//콘텐츠 검색 / 페이징용 리포지토리
 public interface ContentDetailRepositoryCustom {
     Slice<ContentSummaryDto> searchContentDetails(
-            Long smallCategoryId,
+            Long subCategoryId,
             String keyword,
             String artistName,
             String sportTeamName,
             String brandName,
+            String sortBy,
+            Pageable pageable,
+            Long memberId
+    );
+
+    Slice<ContentSummaryDto> findBySubCategory(
+            Long subCategoryId,
             String sortBy,
             Pageable pageable,
             Long memberId
