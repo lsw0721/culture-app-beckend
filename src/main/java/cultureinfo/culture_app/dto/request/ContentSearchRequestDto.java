9340a8acbaffd683aba7dcd05873e1ab0314cd74
LeeSungWon <mindlrae602@gmail.com>
2025-05-29 22:21:33 +0900

    refactor: 소분류 엔티티 삭제

--- src/main/java/cultureinfo/culture_app/dto/request/ContentSearchRequestDto.java
+++ src/main/java/cultureinfo/culture_app/dto/request/ContentSearchRequestDto.java
@@ -1,19 +1,17 @@
 package cultureinfo.culture_app.dto.request;
 
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 //콘텐츠 검색 요청용 dto
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-//보류
+
+
 public class ContentSearchRequestDto {
     //이 정보만 필요
-    private Long smallCategoryId; // 소분류
+    private Long subCategoryId; // 중분류
 
     private String keyword;     // 콘텐츠 이름 검색
     private String artistName; // 가수명 검색 키워드
@@ -22,8 +20,11 @@ public class ContentSearchRequestDto {
     private String sortBy;      // "favoriteCount", "startDateTime" 등
 
     @Min(0)
-    private int   page = 0;         // 0부터 시작
+
+    private int   page = 0;// 0부터 시작
+
     @Min(1)
+
     private int   size = 20;         // 페이지 크기
 
 
