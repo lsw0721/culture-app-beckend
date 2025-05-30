9c3392755286b7c21233bf889184874bfb89b647
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-05-30 18:14:37 +0900

    feat: memberdto에 나이 추가(dto)

--- src/main/java/cultureinfo/culture_app/dto/response/MemberDto.java
+++ src/main/java/cultureinfo/culture_app/dto/response/MemberDto.java
@@ -12,6 +12,7 @@ public class MemberDto {
     private final String name;
     private final String email;
     private final String location;
+    private final Long age;
     private final String gender;
     private final String nickname;
 
@@ -22,6 +23,7 @@ public class MemberDto {
                 .name(member.getName())
                 .email(member.getEmail())
                 .location(member.getLocation())
+                .age(member.getAge())
                 .gender(member.getGender().name())
                 .nickname(member.getNickname())
                 .build();
