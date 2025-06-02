3d22cefe0a7cff23ac27c17857b4e7e94db5e732
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-06-02 15:44:37 +0900

    feat: picture 형식 수정에 따른 dto 수정

--- src/main/java/cultureinfo/culture_app/dto/response/ContentSummaryDto.java
+++ src/main/java/cultureinfo/culture_app/dto/response/ContentSummaryDto.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
 public class ContentSummaryDto {
     private Long id;
     private String contentName; // 콘텐츠 제목
-    private String picture; // 콘텐츠 목록용 사진 url
+    private List<String> imageUrls; // 콘텐츠 목록용 사진 url
     private LocalDateTime startDateTime; // 시작 날짜
     private LocalDateTime endDateTime; // 종료 날짜
 
@@ -19,7 +20,7 @@ public class ContentSummaryDto {
         return ContentSummaryDto.builder()
                 .id(d.getId())
                 .contentName(d.getContentName())
-                .picture(d.getPicture())
+                .imageUrls(d.getImageUrls())
                 .startDateTime(d.getStartDateTime())
                 .endDateTime(d.getEndDateTime())
                 .build();
