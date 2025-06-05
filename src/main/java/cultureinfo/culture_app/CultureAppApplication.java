d304fb5ead93c45d0cc8e35ad7b73c4b46ed57e2
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-06-05 13:27:30 +0900

    feat: 비동기 설정

--- src/main/java/cultureinfo/culture_app/CultureAppApplication.java
+++ src/main/java/cultureinfo/culture_app/CultureAppApplication.java
@@ -2,8 +2,10 @@ package cultureinfo.culture_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CultureAppApplication {
 
 	public static void main(String[] args) {
